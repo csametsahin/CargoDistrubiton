7cfd2db53062f7d8578097168a1759f34a304a81
Cihat Samet Şahin <190201101@kocaeli.edu.tr>
2021-10-25 17:17:45 +0300

    Directions API added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const  initialize =require('firebase/app');
+const getFire  = require('firebase/firestore');
 
 
 
 
+//MAPS YOL BULMADA SIRA
 
 const {app ,BrowserWindow,Menu ,ipcMain}=electron;
 
+const {  initializeApp} = initialize;
+const { getFirestore , collection, addDoc ,getDocs}  = getFire;
+
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyChMr-fShkun_nK9K-ZMRtnrBIRy273EqI',
+  authDomain: 'demomap-329008.firebaseapp.com',
+  projectId: 'demomap-329008'
+});
+
+const db = getFirestore();
+
+var markers = [];
+var lat=[];
+var lng=[];
+var durum=[];
+
 
 let mainWindow;
-
+let addWindow;
 app.on('ready',() => {
         mainWindow= new BrowserWindow({
           webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false 
+            contextIsolation: false ,
+            enableRemoteModule: true
         }
         });
 
@@ -26,7 +47,7 @@ app.on('ready',() => {
             slashes:true
           })
         );
-
+        getFirebaseLocation();
         const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
         Menu.setApplicationMenu(mainMenu);
@@ -37,6 +58,15 @@ app.on('ready',() => {
         ipcMain.on("test:inputValue" ,(err,data)=>{
           console.log(data);
         })
+
+        ipcMain.on("key:latlng",(err,...args)=>{
+         addFirebaseClickedLatLng(args[0],args[1]);
+        })
+
+       
+  
+
+      
 });
 
 const mainMenuTemplate = [
@@ -72,3 +102,53 @@ if(process.env.NOD_ENV !=="production"){
     ]
   });
 }
+/*
+function createWindow(){
+  addWindow = new BrowserWindow({
+    width=482,
+    height=200,
+    title = "Yeni Pencere"
+  });
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname,"login.html"),
+      protocol: "file",
+      slashes:true
+    })
+  );
+}*/
+
+function addFirebaseClickedLatLng(lat,lng){
+  try {
+    const docRef =addDoc(collection(db, "kargo_location"), {
+      location: {lat: lat,long: lng},
+      durum :0
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+const getFirebaseLocation =async function (){
+  try {
+    
+    const querySnapshot = await getDocs(collection(db, "kargo_location"));
+    querySnapshot.forEach((doc) => {
+      if(doc.data().durum == 0){
+        markers.push({coords:doc.data().location , durum : doc.data().durum});
+    
+      }
+    });
+            
+         }
+         catch(e){
+           console.log(e);
+         }
+         finally{
+           mainWindow.webContents.on('did-finish-load',()=>{
+            mainWindow.webContents.send("key:markers",markers);
+           })
+         }
+}
+
---
+++ src/login.js
@@ -0,0 +1,197 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const  initialize =require('firebase/app');
+const getFire  = require('firebase/firestore');
+
+
+
+
+
+
+
+const {app ,BrowserWindow,Menu ,ipcMain}=electron;
+
+const {  initializeApp} = initialize;
+const { getFirestore , collection, addDoc ,doc,getDocs,onSnapshot,updateDoc}  = getFire;
+
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyChMr-fShkun_nK9K-ZMRtnrBIRy273EqI',
+  authDomain: 'demomap-329008.firebaseapp.com',
+  projectId: 'demomap-329008'
+});
+
+const db = getFirestore();
+
+
+let mainWindow;
+var username;
+var password;
+var register; 
+
+app.on('ready',() => {
+        mainWindow= new BrowserWindow({
+          height:1300,
+          width:1000,
+          webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false 
+        }
+        });
+
+        mainWindow.loadURL(
+          url.format({
+            pathname: path.join(__dirname,"login.html"),
+            protocol: "file",
+            slashes:true
+          })
+        );
+
+        const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+        Menu.setApplicationMenu(mainMenu);
+          console.log("Hi");
+     
+        ipcMain.on("key:usernamepass" ,(err,...args)=>{
+         checkFirebaseData(args[0],args[1]);
+        
+      });
+       
+          ipcMain.on("key:regUsernamePass" ,(err,...args)=>{
+           addFireBaseUser(args[0],args[1]);
+             });
+        
+       ipcMain.on("key:kargolocation",(err,...args)=>{
+            addFireBaseLocation(args[0],args[1]);
+       })
+       
+       ipcMain.on("newpass",(err,...args)=>{
+          writeUserData(args[0],args[1],args[2]);
+       })
+
+        
+    
+      
+
+      
+});
+
+const mainMenuTemplate = [
+  {
+    label: "Dosya",
+    submenu : [
+      {
+        label : "Maps Sıfırla"
+      },
+      {
+        label : "Çıkış",
+        accelaretor:process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        role:"quit"
+      }
+    ]
+  }
+]
+
+if(process.env.NOD_ENV !=="production"){
+  mainMenuTemplate.push(  {
+    label : "Dev Tools",
+    submenu: [
+      {
+        label : " Geliştirici Menüsünü Aç ",
+        click(item,focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        label : "Yenile",
+        role : "reload"
+      }
+    ]
+  });
+}
+
+
+const addFireBaseUser = function (username,password){
+
+  try {
+    const docRef =addDoc(collection(db, "users"), {
+      name : username,
+      password: password
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+ const checkFirebaseData =  async function(username,password){
+     try {
+    
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  if(doc.data().name == username && doc.data().password == password){
+    console.log("basarili");
+    newWindow();
+
+  }
+});
+        
+     }
+     catch(e){
+       console.log(e);
+     }
+
+   
+ 
+}
+
+
+function newWindow(){
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname,"main.html"),
+      protocol: "file",
+      slashes:true
+    })
+  );
+
+}
+
+const addFireBaseLocation = function (lt,lg){
+
+  try {
+    const docRef =addDoc(collection(db, "kargo_location"), {
+      location: {lat: lt,long: lg},
+      durum :0
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+const writeUserData= async function (name,oldpass,newpass) {
+  try {
+    
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      if(doc.data().name == name && doc.data().password == oldpass){
+        writeUserCont(doc.id,newpass);
+     
+    
+      }
+    });
+            
+         }
+         catch(e){
+           console.log(e);
+         }
+
+}
+
+const writeUserCont = async function(id,newpass){
+  const adminRef = doc(db, "users", id);
+         updateDoc(adminRef, {
+          password:newpass
+        });
+}
